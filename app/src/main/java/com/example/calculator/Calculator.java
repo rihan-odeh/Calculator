e1040ea558959595f4ae114b73e7c13253798e50
rihan-odeh <73608189+rihan-odeh@users.noreply.github.com>
2021-07-13 12:13:29 +0300

    classes and a tester are added.

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,54 @@
+package com.example.calculator;
+
+
+
+public class Calculator {
+    double operandOne, operandTwo, result;
+
+    char operator;
+
+
+
+    public Calculator(){
+
+    }
+
+    public char getOperator() {
+        return operator;
+    }
+
+    public double getOperandOne() {
+        return operandOne;
+    }
+
+    public double getOperandTwo() {
+        return operandTwo;
+    }
+
+    public void setOperandOne(double operandOne) {
+        this.operandOne = operandOne;
+    }
+
+    public void setOperandTwo(double operandTwo) {
+        this.operandTwo = operandTwo;
+    }
+
+    public void setOperator(char operator) {
+        this.operator = operator;
+    }
+    public void performOperation(){
+        if(getOperator() == '+'){
+           result= getOperandOne() + getOperandTwo();
+        }
+        if(getOperator() == '-'){
+            result= getOperandOne() - getOperandTwo();
+
+        }
+
+    }
+    public double getResult(){
+        performOperation();
+        return result;
+
+    }
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Calculator calculator = new Calculator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        calculator.setOperandOne(10.5);
+        calculator.setOperandTwo(5.2);
+        calculator.setOperator('-');
+        editText.setText(String.valueOf(calculator.getResult()));
+
     }
 }
